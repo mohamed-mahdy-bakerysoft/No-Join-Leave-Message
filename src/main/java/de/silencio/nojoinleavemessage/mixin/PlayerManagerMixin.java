37ae939617dd1b6f56de1aaab2066a29cc974eba
Silencio <silencio@devlencio.net>
2024-08-19 18:59:18 +0200

    Initial Commit

---
+++ src/main/java/de/silencio/nojoinleavemessage/mixin/PlayerManagerMixin.java
@@ -0,0 +1,18 @@
+package de.silencio.nojoinleavemessage.mixin;
+
+import net.minecraft.server.PlayerManager;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerManager.class)
+public class PlayerManagerMixin {
+    @Inject(method = "Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Z)V", at = @At("HEAD"), cancellable = true)
+    public void broadcast(Text message, boolean overlay, CallbackInfo ci) {
+        if (message.toString().contains("multiplayer.player.joined") || message.toString().contains("multiplayer.player.left")) {
+            ci.cancel();
+        }
+    }
+}
